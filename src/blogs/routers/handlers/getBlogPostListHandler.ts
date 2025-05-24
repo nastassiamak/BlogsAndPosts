5551c47676deb27442fe68a91150e97498ea0031
«nastassiamak» <redfox.asya@gmail.com>
2025-05-24 17:06:00 +0300

    add commit 18

--- src/blogs/routers/handlers/getBlogPostListHandler.ts
+++ src/blogs/routers/handlers/getBlogPostListHandler.ts
@@ -4,19 +4,21 @@ import { postService } from "../../../posts/application/postService";
 import { mapToPostListPaginatedOutput } from "../../../posts/routers/mappers/mapToPostListPaginatedOutputUtil";
 import { errorsHandler } from "../../../core/errors/errorsHandler";
 import {setDefaultSortAndPaginationIfNotExist} from "../../../core/helpers/setDefaultSortAndPagination";
+import {blogService} from "../../application/blogService";
 
 export async function getBlogPostListHandler(
   req: Request<{ id: string }, {}, {}, PostQueryInput>,
   res: Response,
 ) {
   try {
-    const blogId = req.params.id;
-    console.log("Получен запрос на blogId:", blogId);  // вывод параметра пути
+    const id = req.params.id;
+    console.log("Получен запрос на blogId:", id);  // вывод параметра пути
 
+    const blogId = await blogService.findByIdOrFail(id);
     const queryInput = setDefaultSortAndPaginationIfNotExist(req.query);
     console.log("Параметры запроса:", queryInput);    // вывод параметров пагинации и сортировки
 
-    const { items, totalCount } = await postService.findPostsByBlog(queryInput, blogId);
+    const { items, totalCount } = await postService.findPostsByBlog(queryInput, blogId._id.toString());
 
     console.log(`Найдено постов: ${items.length}, из них всего: ${totalCount}`);  // вывод результата из БД
 
