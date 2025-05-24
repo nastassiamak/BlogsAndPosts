9f059433fe5d8a5b1e6c76f35b6ed3135bdb3cc1
«nastassiamak» <redfox.asya@gmail.com>
2025-05-24 16:51:00 +0300

    add commit 17/3

--- src/blogs/routers/handlers/getBlogPostListHandler.ts
+++ src/blogs/routers/handlers/getBlogPostListHandler.ts
@@ -11,20 +11,23 @@ export async function getBlogPostListHandler(
 ) {
   try {
     const blogId = req.params.id;
+    console.log("Получен запрос на blogId:", blogId);  // вывод параметра пути
 
-    const queryInput = (req.query);
+    const queryInput = setDefaultSortAndPaginationIfNotExist(req.query);
+    console.log("Параметры запроса:", queryInput);    // вывод параметров пагинации и сортировки
 
+    const { items, totalCount } = await postService.findPostsByBlog(queryInput, blogId);
 
-    const { items, totalCount } = await postService.findPostsByBlog(
-      queryInput,
-      blogId,
-    );
+    console.log(`Найдено постов: ${items.length}, из них всего: ${totalCount}`);  // вывод результата из БД
 
     const postListOutput = mapToPostListPaginatedOutput(items, {
       pageNumber: queryInput.pageNumber,
       pageSize: queryInput.pageSize,
       totalCount,
-    },);
+    });
+
+    console.log("Формируем ответ:", postListOutput);
+
     res.send(postListOutput);
   } catch (e: unknown) {
     errorsHandler(e, res);
