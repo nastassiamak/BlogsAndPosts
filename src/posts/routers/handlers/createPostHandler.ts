3f0b63a88454df7979f05e1d297b3ac6859131a1
«nastassiamak» <redfox.asya@gmail.com>
2025-06-01 11:38:30 +0300

    add commit api-4/10

--- src/posts/routers/handlers/createPostHandler.ts
+++ src/posts/routers/handlers/createPostHandler.ts
@@ -6,14 +6,20 @@ import { HttpStatus } from "../../../core/types/httpStatus";
 import {RepositoryNotFoundError} from "../../../core/errors/repositoryNotFoundError";
 //import { errorsHandler } from "../../../core/errors/errorsHandler";
 
+
 export async function createPostHandler(
-  req: Request<{}, {}, PostCreateInput>,
-  res: Response,
+    req: Request<{ blogId: string }, {}, PostCreateInput>,
+    res: Response,
 ) {
-
     try {
-        const createdPostId = await postService.create(req.body);
+        const blogId = req.params.blogId;
+        const dataToCreate = {
+            ...req.body,
+            blogId, // добавляем blogId из URL в данные поста
+            createdAt: new Date().toISOString(),
+        };
 
+        const createdPostId = await postService.create(dataToCreate);
         const createdPost = await postService.findByIdOrFail(createdPostId);
 
         const postOutput = mapToPostOutput(createdPost);
@@ -21,9 +27,31 @@ export async function createPostHandler(
         res.status(HttpStatus.Created).send(postOutput);
     } catch (error) {
         if (error instanceof RepositoryNotFoundError) {
-            res.status(HttpStatus.NotFound).send({ message: "Post not found" });
+            res.status(HttpStatus.NotFound).send({ message: "Blog or Post not found" });
         } else {
+            console.error(error);
             res.status(HttpStatus.InternalServerError).send({ message: "Internal Server Error" });
         }
     }
 }
+// export async function createPostHandler(
+//   req: Request<{}, {}, PostCreateInput>,
+//   res: Response,
+// ) {
+//
+//     try {
+//         const createdPostId = await postService.create(req.body);
+//
+//         const createdPost = await postService.findByIdOrFail(createdPostId);
+//
+//         const postOutput = mapToPostOutput(createdPost);
+//
+//         res.status(HttpStatus.Created).send(postOutput);
+//     } catch (error) {
+//         if (error instanceof RepositoryNotFoundError) {
+//             res.status(HttpStatus.NotFound).send({ message: "Post not found" });
+//         } else {
+//             res.status(HttpStatus.InternalServerError).send({ message: "Internal Server Error" });
+//         }
+//     }
+// }
