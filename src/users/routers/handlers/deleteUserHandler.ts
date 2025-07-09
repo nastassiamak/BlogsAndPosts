a16a41f6a6268edb728087d3bb532bbf1b5a5ea2
«nastassiamak» <redfox.asya@gmail.com>
2025-07-09 14:20:14 +0300

    add commit api5-12

--- src/users/routers/handlers/deleteUserHandler.ts
+++ src/users/routers/handlers/deleteUserHandler.ts
@@ -2,6 +2,7 @@ import { Request, Response} from "express";
 import {userService} from "../../application/userService";
 import {HttpStatus} from "../../../core/types/httpStatus";
 import {RepositoryNotFoundError} from "../../../core/errors/repositoryNotFoundError";
+import {BusinessRuleError} from "../../../core/errors/businessRuleError";
 
 export async function deleteUserHandler(
     req: Request<{ id: string }>,
@@ -13,8 +14,12 @@ export async function deleteUserHandler(
 
         res.sendStatus(HttpStatus.NoContent);
     } catch (error) {
-        if (error instanceof RepositoryNotFoundError) {
-            res.status(HttpStatus.NotFound).send({ message: "User doesn't exist" });
+        if (error instanceof BusinessRuleError) {
+            res.status(HttpStatus.BadRequest).json({ message: error.message });
+        } else if (error instanceof RepositoryNotFoundError) {
+            res.status(HttpStatus.NotFound).json({ message: "User not found" });
+        } else {
+            res.status(HttpStatus.InternalServerError).send("Internal Server Error");
         }
     }
 }
