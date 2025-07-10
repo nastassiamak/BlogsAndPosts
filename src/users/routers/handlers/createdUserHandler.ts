2f08645ebb84169a3b29a62981fd21c2676b3617
«nastassiamak» <redfox.asya@gmail.com>
2025-07-10 17:26:02 +0300

    add commit api5-24

--- src/users/routers/handlers/createdUserHandler.ts
+++ src/users/routers/handlers/createdUserHandler.ts
@@ -9,7 +9,7 @@ export async function createUserHandler(
     req: Request,
     res: Response,
     ) {
-    try{
+    try {
         const createdUserId = await userService.create(req.body);
         console.log("Created user ID:", createdUserId);
 
@@ -20,13 +20,16 @@ export async function createUserHandler(
         res.status(HttpStatus.Created).send(userOutput);
 
     } catch (error) {
-        console.error("Create user error:", error);
+            console.error("Create user error:", error);
 
-        if (error instanceof BusinessRuleError) {
-            res.status(HttpStatus.BadRequest).json({ message: error.message });
-        } else {
-            res.status(HttpStatus.InternalServerError).send("Internal Server Error");
+            if (error instanceof BusinessRuleError) {
+                res.status(HttpStatus.BadRequest).json({
+                    errorsMessages: error.errors.errorsMessages.length > 0
+                        ? error.errors.errorsMessages
+                        : [{ message: error.message, field: "" }],
+                });
+            }
+
+           res.status(HttpStatus.InternalServerError).send("Internal Server Error");
         }
-    }
-
 }
