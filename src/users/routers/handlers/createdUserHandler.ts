a0d277dd6d8d598556ec03898a3a46833892f4d7
«nastassiamak» <redfox.asya@gmail.com>
2025-07-12 18:53:24 +0300

    add commit api5-30

--- src/users/routers/handlers/createdUserHandler.ts
+++ src/users/routers/handlers/createdUserHandler.ts
@@ -4,9 +4,10 @@ import {HttpStatus} from "../../../core/types/httpStatus";
 import {userService} from "../../application/userService";
 import {BusinessRuleError} from "../../../core/errors/businessRuleError";
 import {RepositoryNotFoundError} from "../../../core/errors/repositoryNotFoundError";
+import {UserCreateInput} from "../input/userCreateInput";
 
 export async function createUserHandler(
-    req: Request,
+    req: Request<{},{}, UserCreateInput>,
     res: Response,
     ) {
     try {
@@ -23,12 +24,11 @@ export async function createUserHandler(
             console.error("Create user error:", error);
 
             if (error instanceof BusinessRuleError) {
-                res.status(HttpStatus.BadRequest).json({
-                    errorsMessages: error.errors.errorsMessages.length > 0
-                        ? error.errors.errorsMessages
-                        : [{ message: error.message, field: "" }],
-                });
+                res.status(HttpStatus.BadRequest).json();
             }
+            // if (error instanceof RepositoryNotFoundError) {
+            //
+            // }
 
            res.status(HttpStatus.InternalServerError).send("Internal Server Error");
         }
