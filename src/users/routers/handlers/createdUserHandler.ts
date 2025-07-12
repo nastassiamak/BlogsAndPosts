26b6fd88934de49a547029de1742006868b7db3b
«nastassiamak» <redfox.asya@gmail.com>
2025-07-12 19:16:51 +0300

    add commit api5-31

--- src/users/routers/handlers/createdUserHandler.ts
+++ src/users/routers/handlers/createdUserHandler.ts
@@ -2,9 +2,8 @@ import { Request, Response } from "express";
 import {mapToUserOutput} from "../mappers/mapToUserOutput";
 import {HttpStatus} from "../../../core/types/httpStatus";
 import {userService} from "../../application/userService";
-import {BusinessRuleError} from "../../../core/errors/businessRuleError";
-import {RepositoryNotFoundError} from "../../../core/errors/repositoryNotFoundError";
 import {UserCreateInput} from "../input/userCreateInput";
+import {BusinessRuleError} from "../../../core/errors/businessRuleError";
 
 export async function createUserHandler(
     req: Request<{},{}, UserCreateInput>,
@@ -14,22 +13,21 @@ export async function createUserHandler(
         const createdUserId = await userService.create(req.body);
         console.log("Created user ID:", createdUserId);
 
-        const createdUser = await userService.findByIdOrFail(createdUserId);
+        const createdUser =
+            await userService.findByIdOrFail(createdUserId);
         console.log("Found user:", createdUser);
 
         const userOutput = mapToUserOutput(createdUser);
-        res.status(HttpStatus.Created).send(userOutput);
+        res.status(HttpStatus.Created).json(userOutput);
 
     } catch (error) {
-            console.error("Create user error:", error);
-
-            if (error instanceof BusinessRuleError) {
-                res.status(HttpStatus.BadRequest).json();
-            }
-            // if (error instanceof RepositoryNotFoundError) {
-            //
-            // }
-
+        if (error instanceof BusinessRuleError) {
+            return res.status(HttpStatus.BadRequest).json({
+                errorsMessages: error.errors.errorsMessages.length > 0
+                    ? error.errors.errorsMessages
+                    : [{ message: error.message, field: '' }],
+            });
+        }
            res.status(HttpStatus.InternalServerError).send("Internal Server Error");
         }
 }
