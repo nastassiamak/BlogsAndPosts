010863f9d12f74a6741ed2fb9f9222170e1c65b1
«nastassiamak» <redfox.asya@gmail.com>
2025-07-12 16:45:01 +0300

    add commit api5-29

--- src/db/mongoDb.ts
+++ src/db/mongoDb.ts
@@ -19,21 +19,16 @@ export async function runDB(url: string): Promise<void> {
   client = await MongoClient.connect(url);
   const db: Db = client.db(SETTINGS.DB_NAME);
 
-  // Инициализация коллекций
+  //Инициализация коллекций
   blogCollection = db.collection<Blog>(BLOG_COLLECTION_NAME);
   postCollection = db.collection<Post>(POST_COLLECTION_NAME);
   userCollection = db.collection<User>(USER_COLLECTION_NAME);
+
 
   try {
     await client.connect();
     await db.command({ ping: 1 });
     console.log("✅ Connected to the database");
-
-    // Создаем уникальные индексы для пользователей
-    await userCollection.createIndex({ login: 1 }, { unique: true });
-    await userCollection.createIndex({ email: 1 }, { unique: true });
-    console.log("✅ Unique indexes on login and email created");
-
   } catch (e) {
     await client.close();
     throw new Error(`❌ Database not connected: ${e}`);
