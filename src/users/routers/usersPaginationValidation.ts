5b9962ddac6dedd71742e7f67174c2349068e401
«nastassiamak» <redfox.asya@gmail.com>
2025-07-13 13:17:18 +0300

    add commit api5-49

--- src/users/routers/usersPaginationValidation.ts
+++ src/users/routers/usersPaginationValidation.ts
@@ -1,7 +1,8 @@
 import {query} from "express-validator";
 
 export const usersPaginationValidation = [
-    query('searchNameTerm').optional().isString(),
+    query('searchLoginTerm').optional().isString(),
+    query('searchEmailTerm').optional().isString(),
     query('sortBy').optional().default('createdAt').isIn(['createdAt']),
     query('sortDirection').optional().default('desc').isIn(['asc', 'desc']),
     query('pageNumber').optional().default(1).isInt({ min: 1 }).toInt(),
