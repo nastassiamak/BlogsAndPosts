67ca8f40bd9410800de0b87634f6c0ee30978fde
«nastassiamak» <redfox.asya@gmail.com>
2025-07-19 16:18:28 +0300

    add commit api6-6

--- src/auth/loginHandler.ts
+++ src/auth/loginHandler.ts
@@ -5,31 +5,37 @@ import {userService} from "../users/application/userService";
 import {jwtService} from "./JWT/jwtService";
 import {mapToUserOutput} from "../users/routers/mappers/mapToUserOutput";
 
+
 export async function loginHandler(req: Request, res: Response) {
   try {
-      const { loginOrEmail, password } = req.body;
+    const { loginOrEmail, password } = req.body;
 
-        const isValid = await authService.checkCredentials(loginOrEmail, password);
+    const isValid = await authService.checkCredentials(loginOrEmail, password);
 
-        if (!isValid) {
-          res.status(HttpStatus.Unauthorized).json({message: "Unauthorized"});
-        }
-
-        const user = await userService.findByLoginOrEmail(loginOrEmail);
-        if (!user) {
-          res.status(HttpStatus.Unauthorized).json({message: "Unauthorized"});
-        }
-
-        const userDto = mapToUserOutput(user);
-
-        const token = jwtService.generateToken({
-          userId: userDto.id,
-          login: loginOrEmail,
-          email: userDto.email,
-        });
-    res.status(HttpStatus.Ok).json({accessToken: token});
-
-    } catch (error) {
-      res.status(HttpStatus.InternalServerError).json({message: "internal server error"});
+    if (!isValid) {
+      res.status(HttpStatus.Unauthorized).json({ message: "Unauthorized" });
+      return;
     }
+
+    const user = await userService.findByLoginOrEmail(loginOrEmail);
+    if (!user) {
+       res.status(HttpStatus.Unauthorized).json({ message: "Unauthorized" });
+       return;
+    }
+
+    const userDto = mapToUserOutput(user);
+
+    const token = jwtService.generateToken({
+      userId: userDto.id,
+      login: loginOrEmail,
+      email: userDto.email,
+    });
+
+     res.status(HttpStatus.Ok).json({ accessToken: token });
+
+  } catch (error) {
+    console.error(error);
+    res.status(HttpStatus.InternalServerError).json({ message: "internal server error" });
+    return;
+  }
 }
