595f189364689c7d6df5a2f009967cfe2bac42d8
«nastassiamak» <redfox.asya@gmail.com>
2025-07-27 13:05:04 +0300

    add commit api6-24

--- src/auth/loginHandler.ts
+++ src/auth/loginHandler.ts
@@ -3,6 +3,7 @@ import { HttpStatus } from "../core/types/httpStatus";
 import {userService} from "../users/application/userService";
 import {jwtService} from "./JWT/jwtService";
 import {mapToUserOutput} from "../users/routers/mappers/mapToUserOutput";
+import {BusinessRuleError} from "../core/errors/businessRuleError";
 
 
 export async function loginHandler(req: Request, res: Response) {
@@ -25,8 +26,15 @@ export async function loginHandler(req: Request, res: Response) {
 
     res.status(HttpStatus.Ok).json({ accessToken: token });
 
-  } catch (error) {
-    console.error(error);
-    res.status(HttpStatus.Unauthorized).json({ message: "internal server error" });
+  }  catch (error) {
+    if (error instanceof BusinessRuleError) {
+      res.status(HttpStatus.BadRequest).json({
+        errorsMessages:
+            error.errors.errorsMessages.length > 0
+                ? error.errors.errorsMessages
+                : [{ message: error.message, field: "" }],
+      });
+    }
+    res.status(HttpStatus.InternalServerError).send("Internal Server Error");
   }
 }
