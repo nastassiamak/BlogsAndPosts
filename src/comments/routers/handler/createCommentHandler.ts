5ac229176cc8350ee3d4b68017c6d893d29871a6
«nastassiamak» <redfox.asya@gmail.com>
2025-07-29 11:54:52 +0300

    add commit api6-30

--- src/comments/routers/handler/createCommentHandler.ts
+++ src/comments/routers/handler/createCommentHandler.ts
@@ -6,46 +6,38 @@ import { commentService } from "../../application/commentService";
 import { HttpStatus } from "../../../core/types/httpStatus";
 import { RepositoryNotFoundError } from "../../../core/errors/repositoryNotFoundError";
 import {CommentAttributes} from "../../application/dto/commentAttributes";
-import {postsRepository} from "../../../posts/repositories/postsRepository";
-import {commentsRepository} from "../repositories/commentsRepository";
 
 export async function createCommentHandler(
     req: Request, res: Response){
   try {
     const user = req.user;  // должен быть установлен в authMiddleware
     if (!user) {
-       res.status(401).json({ message: "Unauthorized" });
-      return;
-    }
-    const { postId } = req.params;
-    const { content } = req.body;
-
-
-    const post = await postService.findByIdOrFail(postId);
-    if (!post) {
-      res.status(404).json({ message: "Post not found" });
+      return res.status(401).json({ message: "Unauthorized" });
     }
 
-    const comment = await commentService.create({
-      content: content,
-      commentatorInfo: {
-        userId: user._id.toString(),
-        userLogin: user.login,
-      }
-      // commentatorInfo: {
-      //   userId: string;
-      //   userLogin?: string;
-      // } // если не приходит с клиента, не обязательно
-      // createdAt?: string;
-    });
+    const postId = req.params.postId;
+    if (!postId) {
+       res
+           .status(HttpStatus.BadRequest)
+           .json({ errorsMessages: [{ field: 'postId', message: 'PostId is required' }] });
+    }
 
+    const { content, commentatorInfo } = req.body as CommentAttributes;
 
-    //const createdCommentId = await commentService.create(postId, commentInput);
-   // const createdComment = await commentService.findByIdOrFail(createdCommentId);
+    await postService.findByIdOrFail(postId);
 
-   // const commentOutput = mapToCommentOutput(createdComment);
+    const commentInput = {
+      content,
+    commentatorInfo,
 
-     res.status(HttpStatus.Created).send(comment);
+    };
+
+    const createdCommentId = await commentService.create(commentInput);
+    const createdComment = await commentService.findByIdOrFail(createdCommentId);
+
+    const commentOutput = mapToCommentOutput(createdComment);
+
+     res.status(HttpStatus.Created).send(commentOutput);
   } catch (error) {
     console.error("Error in createCommentHandler:", error);
     if (error instanceof RepositoryNotFoundError) {
