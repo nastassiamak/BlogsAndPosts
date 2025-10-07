eee8999bc29558cf15aec8dc392e21197d72bd94
«nastassiamak» <redfox.asya@gmail.com>
2025-10-07 12:40:42 +0300

    add commit api9

--- src/posts/repositories/postsRepository.ts
+++ src/posts/repositories/postsRepository.ts
@@ -29,7 +29,7 @@ export const postsRepository = {
 
     const rawItems = await postCollection
       .find(filter)
-      .sort({ [sortBy]: direction })
+      .sort({ [sortBy]: direction, _id: 1  })
       .skip(skip)
       .limit(pageSize)
       .toArray();
